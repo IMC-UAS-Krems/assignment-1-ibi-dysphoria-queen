5d4728f3dbd2f1648c8c61441fd3db3bd7159178
ibi-dysphoria-queen <ibolyavojtek@gmail.com>
2026-04-06 21:47:14 +0200

    added tracks

--- src/streaming/tracks.py
+++ src/streaming/tracks.py
@@ -14,3 +14,39 @@ Classes to implement:
     - AudiobookTrack
 """
 
+class Track:
+    def __init__(self,track_id,title,duration_seconds,genre):
+        self.track_id = track_id
+        self.title = title
+        self.duration_seconds = duration_seconds
+        self.genre = genre
+    def duration_minutes(self):
+        return self.duration_seconds/60
+class Podcast(Track):
+    def __init__(self,track_id,title,duration_seconds,genre,host,description):
+        super().__init__(track_id,title,duration_seconds,genre)
+        self.host = host
+        self.description = description
+class NarrativeEpisode(Podcast):
+    def __init__(self,track_id,title,duration_seconds,genre,host,description,season,episode_number):
+        super().__init__(track_id,title,duration_seconds,genre,host,description)
+        self.season = season
+        self.episode_number = episode_number
+class InterviewEpisode(Podcast):
+    def __init__(self,track_id,title,duration_seconds,genre,host,description,guest):
+        super().__init__(track_id,title,duration_seconds,genre,host,description)
+        self.guest = guest
+class Song(Track):
+    def __init__(self,track_id,title,duration_seconds,genre,artist):
+        super().__init__(track_id,title,duration_seconds,genre)
+        self.artist = artist
+class AlbumTrack(Song):
+    def __init__(self,track_id,title,duration_seconds,genre,artist,track_number,album):
+        super().__init__(track_id,title,duration_seconds,genre,artist)
+        self.track_number = track_number
+        self.album = album
+class SingleRelease(Song):
+    def __init__(self,track_id,title,duration_seconds,genre,artist,release_date):
+        super().__init__(track_id,title,duration_seconds,genre,artist)
+        self.release_date = release_date
+
